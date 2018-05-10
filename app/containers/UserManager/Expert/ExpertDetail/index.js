583afc6d009523e6f23e6c84f993047ca1f19f68
VietUngIT <vietungit140494@gmail.com>
2018-05-10 18:51:40 +0700

    ----

--- app/containers/UserManager/Expert/ExpertDetail/index.js
+++ app/containers/UserManager/Expert/ExpertDetail/index.js
@@ -13,6 +13,7 @@ import CusInput from 'components/Utils/CusInput';
 import Tags from 'components/Utils/Tags';
 import CusSelect from 'components/Utils/CusSelect';
 import styles from './styles';
+import CexpertInfoDetail from 'components/CUserManager/CExpert/CexpertInfoDetail'
 import {
   getListField,
   getExpertDetail,
@@ -76,16 +77,36 @@ export class ExpertDetail extends React.Component {
     }
     
   }
-  capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  addTags=(value)=>{
+    var temp = this.state.listTags;
+    temp.push(value)
+    this.setState({listTags: temp})
   }
-  addDegree=()=>{
-    if(this.degreeExpert.value && this.degreeExpert.value.trim()!==""){
-      var temp = this.state.degree;
-      temp.push(this.degreeExpert.value.trim())
-      this.setState({degree: temp})
-      this.degreeExpert.value="";
+  removeTags=(idx)=>{
+    this.setState({
+      listTags: this.state.listTags.filter((item,index)=>{
+        if(index!==idx) return item;
+      })
+    })
+  }
+  updateTags=()=>{
+    if(this.props.expert){
+      this.props.updateTags(this.state.listTags,this.props.expert.phone)
     }
+  }
+  cancelTags=()=>{
+    if(this.props.expert){
+      let tempTags = JSON.parse(this.props.expert.tags);
+      this.setState({
+        listTags: tempTags,
+      })
+    }
+  }
+
+  addDegree=(value)=>{
+    var temp = this.state.degree;
+    temp.push(value)
+    this.setState({degree: temp})
   }
   removeDegree=(idx)=>{
     this.setState({
@@ -115,24 +136,24 @@ export class ExpertDetail extends React.Component {
     this.setState({stateEditDegree: false,})
   }
 
-  addField=()=>{
+  addField=(value)=>{
     let name = "";
     if(this.props.listSubField && (this.props.listSubField.size>0 || this.props.listSubField.length>0)){
       this.props.listSubField.map((item, index) => {
-        if(item.id===this.selectField.value){
+        if(item.id===value){
           name = item.nameField;   
         }
       })
     }
     var i = 0;
     this.state.listFieldDetail.map((item,index)=>{
-      if(item.id===this.selectField.value){
+      if(item.id===value){
         i++;
       }
     })
     if(i===0){
       var temp = this.state.listFieldDetail;
-      temp.push({id:this.selectField.value, idParentField: this.props.expert.idParentField, nameField:name})
+      temp.push({id:value, idParentField: this.props.expert.idParentField, nameField:name})
       this.setState({listFieldDetail: temp})
     }
     
@@ -171,213 +192,22 @@ export class ExpertDetail extends React.Component {
     this.setState({stateEditFieldDetail: false,})
   }
 
-  addTags=()=>{
-    if(this.tagsExpert.value && this.tagsExpert.value.trim()!==""){
-      var temp = this.state.listTags;
-      temp.push(this.tagsExpert.value.trim())
-      this.setState({listTags: temp})
-      this.tagsExpert.value="";
-    }
-  }
-  removeTags=(idx)=>{
+  showProfile=()=>{
     this.setState({
-      listTags: this.state.listTags.filter((item,index)=>{
-        if(index!==idx) return item;
-      })
-    })
-  }
-  updateTags=()=>{
-    if(this.props.expert){
-      this.props.updateTags(this.state.listTags,this.props.expert.phone)
-    }
-  }
-  cancelTags=()=>{
-    if(this.props.expert){
-      let tempTags = JSON.parse(this.props.expert.tags);
-      this.setState({
-        listTags: tempTags,
-      })
-    }
+      showProfile: true,
+    });
   }
   render() {
-    let name = "";
-    let avatar = require('containers/App/fuse.svg');
-    let rate = 0;
-    let widthRate = 0;
-    let numRate = 0;
-    let online = false;
-    let phone = "";
-    let address = "";
-    let email = "";
-    let desc = "";
-    let workPlace = "";
-    let fieldName = "";
-    let detaiField = "";
-    let degree = "";
-    let listDegree = false;
-    let showListField = false;
-    let dropSubField = false;
-    let showTags = false;
-    if(this.props.expert){
-      name = this.props.expert.name;
-      if(this.props.expert.avatar!==null){
-        avatar = this.props.expert.avatar;
-      }
-      rate = this.props.expert.rate;
-      widthRate = Math.floor((this.props.expert.rate/5)*100);
-      numRate = this.props.expert.numRate;
-      online = this.props.expert.isOnline;
-      phone = this.props.expert.phone;
-      address = this.props.expert.address;
-      email = this.props.expert.email;
-      if(this.props.expert.desc!==null){
-        desc = this.props.expert.desc;
-      }
-      workPlace = this.props.expert.workPlace;
-      if(this.props.expert.arrayField && (this.props.expert.arrayField.size>0 || this.props.expert.arrayField.length>0)){
-        this.props.expert.arrayField.map((item,index)=>{
-          if(index!==0){
-            detaiField += (", "+this.capitalizeFirstLetter(item.nameField));
-          }else{
-            detaiField = this.capitalizeFirstLetter(item.nameField);
-          }
-          
-        })
-      }
-      let tempDegree = JSON.parse(this.props.expert.degree);
-      if(tempDegree && (tempDegree.size>0 || tempDegree.length>0)){
-        tempDegree.map((item,index)=>{
-          if(index!==0){
-            degree += (", "+this.capitalizeFirstLetter(item));
-          }else{
-            degree = this.capitalizeFirstLetter(item);
-          }
-        })
-      }
-
-      if(this.props.listSubField && (this.props.listSubField.size>0 || this.props.listSubField.length>0)){
-        dropSubField = this.props.listSubField.map((item, index) => {
-          if(item.idParentField===this.props.expert.idParentField){
-            return (<option value={item.id}  key={index}>{item.nameField}</option>)    
-          }
-        })
-      }
-    }
-    if(this.state.degree && (this.state.degree.size>0 || this.state.degree.length>0)){
-      listDegree = this.state.degree.map((item,index)=>{
-        return <Tags content={item} key={index} index={index} removeTags={this.removeDegree}/>
-      });
-    }
-    if(this.state.listFieldDetail && (this.state.listFieldDetail.size>0 || this.state.listFieldDetail.length>0)){
-      showListField = this.state.listFieldDetail.map((item,index)=>{
-        return <Tags content={item.nameField} key={index} index={item.id} removeTags={this.removeField}/>
-      });
-    }
-    if(this.state.listTags && (this.state.listTags.size>0 || this.state.listTags.length>0)){
-      showTags = this.state.listTags.map((item,index)=>{
-        return <Tags content={item} key={index} index={index} removeTags={this.removeTags}/>
-      });
-    }
-
-    if(this.props.listField && (this.props.listField.size>0 || this.props.listField.length>0)){
-      this.props.listField.map((item,index)=>{
-        if(item.id === this.props.expert.idParentField){
-          fieldName = item.name;
-        }
-      })
-    }
 
     let showProfileHTML = false;
     if(this.props.expert){
-      showProfileHTML = (
-        <div style={{display: this.state.showProfile?"":'none'}}>
-          <div >
-            <div style={styles.wrapDetail}>
-              <div style={styles.wrapTitle}>Thông tin chung</div>
-              <div style={styles.wrapBody}>
-                <div style={styles.label}>Số điện thoại</div>
-                <div style={styles.value}>{phone}</div>
-                <div style={styles.label}>Địa chỉ</div>
-                <div style={styles.value}>{address}</div>
-                <div style={styles.label}>Email</div>
-                <div style={styles.value}>{email}</div>
-                <div >{desc}</div>
-              </div>
-            </div>
-            <div style={styles.wrapDetail}>
-              <div style={styles.wrapTitle}>Nghề nghiệp</div>
-              <div style={styles.wrapBody}>
-                <div style={styles.label}>Nơi làm việc</div>
-                <div style={styles.value}>{workPlace}</div>
-                <div style={styles.label}>Lĩnh vực làm việc</div>
-                <div style={styles.value}>{fieldName}</div>
-                <div style={styles.labelWork}>Chi tiết lĩnh vực làm việc</div>
-                <Icon onClick={this.editField} type="edit" style={styles.iconEdit} />
-                <div style={{display:this.state.stateEditFieldDetail?'':"none"}}>
-                  <div style={{display: 'flex'}}>
-                    <div style={{flex: 1}}>
-                    <CusSelect type='text' innerRef={(comp) => { this.selectField = comp;}} ref="selectFieldExpert">
-                      {dropSubField}
-                    </CusSelect>
-                    </div>
-                    <div style={{flexBasis:100,marginTop: 3,marginLeft: 5}}>
-                      <Button onClick={this.addField}>Thêm</Button>
-                    </div>
-                  </div>
-                  <div>{showListField}</div>
-                  <div style={{textAlign: 'end', paddingRight: 25, marginTop: 10}}>
-                    <Button onClick={this.updateField} type="primary" icon="reload" >Cập nhật</Button>
-                    <Button onClick={this.cancleField} type="danger" icon="close" style={{marginLeft: 20}} >Hủy</Button>
-                  </div>
-                </div>
-                <div style={{display:this.state.stateEditFieldDetail?'none':""}}>
-                  <div style={styles.value}>{detaiField}</div>
-                </div>
-                <div style={styles.labelWork}>Bằng cấp</div>
-                <Icon onClick={this.editDegree} type="edit" style={styles.iconEdit} />
-                <div style={{display:this.state.stateEditDegree?'':"none"}}>
-                  <div style={{display: 'flex'}}>
-                    <div style={{flex: 1}}>
-                      <CusInput type='text' innerRef={(comp) => { this.degreeExpert = comp;}} placeholder="Nhập bằng cấp"/>
-                    </div>
-                    <div style={{flexBasis:100,marginTop: 3,marginLeft: 5}}>
-                      <Button onClick={this.addDegree}>Thêm</Button>
-                    </div>
-                  </div>
-                  <div>{listDegree}</div>
-                  <div style={{textAlign: 'end', paddingRight: 25, marginTop: 10}}>
-                    <Button onClick={this.updateDegree} type="primary" icon="reload" >Cập nhật</Button>
-                    <Button onClick={this.cancleDegree} type="danger" icon="close" style={{marginLeft: 20}} >Hủy</Button>
-                  </div>
-                </div>
-                <div style={{display:this.state.stateEditDegree?'none':""}}>
-                  <div style={styles.value}>{degree}</div>
-                </div>
-              </div>
-            </div>
-            <div style={styles.wrapDetail}>
-              <div style={styles.wrapTitle}>Từ khóa tìm kiếm</div>
-              <div style={styles.wrapBody}>
-              <div >
-                  <div style={{display: 'flex'}}>
-                    <div style={{flex: 1}}>
-                      <CusInput type='text' innerRef={(comp) => { this.tagsExpert = comp;}} placeholder="Nhập từ khóa"/>
-                    </div>
-                    <div style={{flexBasis:100,marginTop: 3,marginLeft: 5}}>
-                      <Button onClick={this.addTags}>Thêm</Button>
-                    </div>
-                  </div>
-                  <div>{showTags}</div>
-                  <div style={{textAlign: 'end', paddingRight: 25, marginTop: 10}}>
-                    <Button onClick={this.updateTags} type="primary" icon="reload" >Cập nhật</Button>
-                    <Button onClick={this.cancelTags} type="danger" icon="close" style={{marginLeft: 20}} >Hủy</Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )
+      showProfileHTML = (<CexpertInfoDetail expert={this.props.expert} listField={this.props.listField} listSubField={this.props.listSubField}
+                          updateSubField={this.props.updateSubField} updateDegree={this.props.updateDegree} updateTags={this.props.updateTags}
+                          showProfile={this.state.showProfile} listTags={this.state.listTags} addTags={this.addTags} updateTags={this.updateTags}
+                          removeTags={this.removeTags} cancelTags={this.cancelTags} degree={this.state.degree} addDegree={this.addDegree} updateDegree={this.updateDegree}
+                          removeDegree={this.removeDegree} cancleDegree={this.cancleDegree} editDegree={this.editDegree} stateEditDegree={this.state.stateEditDegree}
+                          listFieldDetail={this.state.listFieldDetail} addField={this.addField} updateField={this.updateField}
+                          removeField={this.removeField} cancleField={this.cancleField} editField={this.editField} stateEditFieldDetail={this.state.stateEditFieldDetail}/>)
     }
     
     let loading = null;
@@ -387,6 +217,22 @@ export class ExpertDetail extends React.Component {
           <img src={require('containers/App/loading.gif')} style={styles.imageLoading}/>
         </div>
       )
+    }
+    let avatar = require('containers/App/fuse.svg');
+    let online = false;
+    let rate = 0;
+    let widthRate = 0;
+    let numRate = 0;
+    let name = "";
+    if(this.props.expert){
+      if(this.props.expert.avatar!==null){
+        avatar = this.props.expert.avatar;
+      }
+      online = this.props.expert.isOnline;
+      rate = this.props.expert.rate;
+      widthRate = Math.floor((this.props.expert.rate/5)*100);
+      numRate = this.props.expert.numRate;
+      name = this.props.expert.name;
     }
     return (
       <div style={{height: '100%'}}>
@@ -423,8 +269,8 @@ export class ExpertDetail extends React.Component {
                 </div>
               </div>
             </div>
-            <div style={styles.wrapItem}>
-              Thông tin cá nhân
+            <div style={styles.wrapItem} onClick={this.showProfile}>
+              <div style={{color: this.state.showProfile?"#2962FF":"#000000"}}>Thông tin cá nhân</div>
             </div>
           </Col>
           <Col span={17}>
